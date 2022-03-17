b3932335c4ca51ece5f44bc683812c21bb38dc37
29ab <87421954+29ab@users.noreply.github.com>
2022-03-17 15:44:26 +0530

    Update MoviesDashboard.jsx

--- src/components/MoviesDashboard.jsx
+++ src/components/MoviesDashboard.jsx
@@ -7,54 +7,50 @@ const genres = ["Comedy", "Thriller", "Drama", "Documentary", "Children"];
 
 
 export const MoviesDashboard = () => {
-  
+
   // const  dispatch = useDispatch()
-  const [lists,setLists] = useState([]);
-  
+  const [lists, setLists] = useState([]);
+
   //   // to get all movies list on component mounts
 
   useEffect(() => {
     //   dispatch an action to the store 
 
     axios.get("https://movie-fake-server.herokuapp.com/data")
-  .then((res) => setLists(res.data))
-  .catch((err) => console.log(err));
+      .then((res) => setLists(res.data))
+      .catch((err) => console.log(err));
   }, []);
 
   // console.log("Movies",lists)
 
-//    filter by genre 
-const handleFilter = (e)=>{
+  //    filter by genre 
+  const handleFilter = (e) => {
     // dispach filterby genre action to the store
-}
+  }
   return (
     <>
       <h2>Movies</h2>
-      <select  onChange={handleFilter} > 
-      <h6>Movie</h6>
-        {/* map through the filter  */}
-        {
-          genres.map((e) => {
-           return(
-            <h6>{e}</h6>
-          )})
-        }
 
-       </select>
-      <div className = "movies-list">
-        
+      <select style={{marginBottom:"100px"}} >
+        <p>Select</p>
+        {genres.map((e) => {
+          return <option>{e}</option>;
+        })}
+      </select>
+      <div className="movies-list">
+
         {
           lists.map((elem) => {
-            return(
+            return (
               <div key={elem.id}>
                 <h4> {elem.movie_name}</h4>
-               <img src={elem.image_url} />
-               <p>{elem.genre}</p>
+                <img src={elem.image_url} />
+                <p>{elem.genre}</p>
               </div>
             )
           })
         }
-       {/* map throught th movie list and display the results */}
+        {/* map throught th movie list and display the results */}
       </div>
     </>
   );
